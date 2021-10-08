87954e175098afacb5cbe50cee926fb772768cab
Pavlo Bashynskiy <levonet@gmail.com>
2021-10-08 19:55:17 +0300

    test prototype

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core')
 const { run, cleanup, isPost, setPost } = require('./lib/action')
 
-console.log(process.env)
 if (!isPost(core)) {
     setPost(core)
     run(core)
